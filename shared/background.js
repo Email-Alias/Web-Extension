dc1167e2fdc3a6b1299cafa397edac9dacc3e0df
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2025-07-03 21:29:12 +0200

    reverse change in background.js, update content.js

--- shared/background.js
+++ shared/background.js
@@ -1,18 +1,8 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.type === "alias") {
-        (async () => {
-          await chrome.action.setPopup({
-            popup: 'popover.html',
-            tabId: sender.tab.id
-          });
-          await chrome.action.openPopup();
-	  await chrome.action.setPopup({
-            popup: 'src/index.html',
-            tabId: sender.tab.id
-          });
-          const json = await chrome.runtime.sendNativeMessage('com.opdehipt.email_alias', {type: 'getAliases'});
+        chrome.runtime.sendNativeMessage('com.opdehipt.email_alias', {type: 'getAliases'}).then((json) => {
           sendResponse(json);
-        })()
+        });
         return true;
     }
     return false;
