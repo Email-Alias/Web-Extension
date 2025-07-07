9405ae20856822e3fae81bead058be623fe1c420
Sven Op de Hipt <TTnAf20PugxtDi.FOBmT@opdehipt.com>
2025-07-07 23:02:33 +0200

    update content.js to add per character type

--- shared/content.js
+++ shared/content.js
@@ -1,7 +1,23 @@
+function replaceAndType(text, delay = 10) {
+  text.split('').forEach((char, idx) => {
+    setTimeout(() => {
+      document.execCommand('insertText', false, char);
+    }, delay * idx);
+  });
+}
+
+async function getEmails() {
+    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    if (isSafari) {
+        return await browser.runtime.sendMessage({ type: "alias" });
+    }
+    return await chrome.runtime.sendMessage({ type: "alias" });
+}
+
 document.addEventListener('focus', async e => {
-    if (e.target.matches("input[type='email']")) {
+    if (e.target.matches("input[type='email']") && !e.target.value) {
         const currentDomain = document.URL.split('/')[2].split('.').reverse()[1];
-        let emails = JSON.parse((await chrome.runtime.sendMessage({ type: "alias" })).emails);
+        let emails = JSON.parse((await getEmails()).emails);
         let selectedEmails = []
         for (let email of emails) {
             if (email.privateComment.replace(' ', '').toLowerCase().includes(currentDomain)) {
@@ -9,10 +25,7 @@ document.addEventListener('focus', async e => {
             }
         }
         if (selectedEmails.length == 1) {
-            const inputs = document.querySelectorAll("input[type='email']");
-            for (let input of inputs) {
-                input.value = selectedEmails[0].address;
-            }
+            replaceAndType(selectedEmails[0].address);
         }
     }
 }, true);
